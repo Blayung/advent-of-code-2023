f444bb01026526b9c5f8cf54997540c45e3d2efb
Blayung <gpewojan1@gmail.com>
2023-12-01 20:25:23 +0100

    solution to the second half of the 1st problem

--- 1/main.py
+++ 1/main.py
@@ -1,19 +1,49 @@
 #!/bin/python3
-lines = [list(i) for i in open('data.txt', 'r').readlines()]
-result = 0
-for line in lines:
-    for char in line:
-        try:
-            firstDigit = int(char)
-            break
-        except ValueError:
-            pass
-    for char in reversed(line):
-        try:
-            secondDigit = int(char)
-            break
-        except ValueError:
-            pass
-    result += firstDigit * 10 + secondDigit
+digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+def convert(num):
+    if num == '0':
+        return 0
+    elif num == '1' or num == 'one':
+        return 1
+    elif num == '2' or num == 'two':
+        return 2
+    elif num == '3' or num == 'three':
+        return 3
+    elif num == '4' or num == 'four':
+        return 4
+    elif num == '5' or num == 'five':
+        return 5
+    elif num == '6' or num == 'six':
+        return 6
+    elif num == '7' or num == 'seven':
+        return 7
+    elif num == '8' or num == 'eight':
+        return 8
+    elif num == '9' or num == 'nine':
+        return 9
 
+result = 0
+for line in open('data.txt', 'r').readlines():
+    i = 1
+    while True:
+        for digit in digits:
+            if digit in line[:i]:
+                result += convert(digit) * 10
+                break
+        else:
+            i += 1
+            continue
+        break
+
+    i = -1
+    while True:
+        for digit in digits:
+            if digit in line[i:]:
+                result += convert(digit)
+                break
+        else:
+            i -= 1
+            continue
+        break
+    
 print(result)
