3f2df9989302f9edff1700b4927e9776c8aa5673
Blayung <gpewojan1@gmail.com>
2023-12-02 18:09:32 +0100

    solved the second half of the 2nd problem

--- 2/main.py
+++ 2/main.py
@@ -7,12 +7,29 @@ for line in open('data.txt', 'r').readlines():
         game.append((part[1].rstrip('\n'), int(part[0])))
     games.append(game)
 
-result = 0
-for game_id, cubes in enumerate(games, 1):
-    for cube in cubes:
-        if (cube[0] == 'red' and cube[1] > 12) or (cube[0] == 'green' and cube[1] > 13) or (cube[0] == 'blue' and cube[1] > 14):
-            break
-    else:
-        result += game_id
+result1 = 0
+result2 = 0
 
-print(result)
+for game_id, cubes in enumerate(games, 1):
+    red_max = 0
+    green_max = 0
+    blue_max = 0
+
+    checkIfItsEnough = True
+
+    for cube in cubes:
+        if cube[0] == 'red' and cube[1] > red_max:
+            red_max = cube[1]
+        if cube[0] == 'green' and cube[1] > green_max:
+            green_max = cube[1]
+        if cube[0] == 'blue' and cube[1] > blue_max:
+            blue_max = cube[1]
+
+        if checkIfItsEnough:
+            if (cube[0] == 'red' and cube[1] > 12) or (cube[0] == 'green' and cube[1] > 13) or (cube[0] == 'blue' and cube[1] > 14):
+                result1 += game_id
+                checkIfItsEnough = False
+
+    result2 += red_max * green_max * blue_max
+
+print(f'{result1}\n{result2}')
