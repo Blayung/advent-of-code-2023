74648bf347d098028609e924fbb4c38de4837922
Blayung <gpewojan1@gmail.com>
2023-12-04 22:19:11 +0100

    actually solved the second half of the 4th puzzle!

--- 4/part2.py
+++ 4/part2.py
@@ -1,33 +1,30 @@
 #!/bin/python3
-ogCards = [line.split()[2:] for line in open('data.txt', 'r').readlines()]
+nextCards = []
+for cardNum, card in enumerate([line.split()[2:] for line in open('data.txt', 'r').readlines()]):
+    correctNumsAmount = 0
+    winningNums = []
+    leftOrRight = True
+    for part in card:
+        if part == '|':
+            leftOrRight = False
+            continue
+        if leftOrRight:
+            winningNums.append(part)
+        elif part in winningNums:
+            correctNumsAmount += 1
+    nextCards.append(range(cardNum + 1, cardNum + correctNumsAmount + 1))
 
 result = 0
-cards = ogCards
-while True:
-    result += len(cards)
 
-    cardsToCopy = []
-    for cardNum, card in enumerate(cards, 1):
-        wonCards = 0
-        winningNums = []
-        leftOrRight = True
-        for part in card:
-            if part == '|':
-                leftOrRight = False
-                continue
-            if leftOrRight:
-                winningNums.append(part)
-            elif part in winningNums:
-                wonCards += 1
-        cardsToCopy.extend(range(cardNum, cardNum + wonCards))
-
-    if len(cardsToCopy) == 0:
-        print(result)
-        break
-
-    cards = []
-    for cardToCopy in cardsToCopy:
+def processCards(cards):
+    global result
+    for card in cards:
         try:
-            cards.append(ogCards[cardToCopy])
+            processCards(nextCards[card])
+            result += 1
         except IndexError:
             break
+
+processCards(range(0, len(nextCards)))
+
+print(result)
