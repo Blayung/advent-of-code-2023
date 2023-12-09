27eb509e44c0c819bddd126030815deab393e7cc
Blayung <gpewojan1@gmail.com>
2023-12-09 20:09:07 +0100

    more fixes to the solution of the first half of the 7th problem (still doesn't work though)

--- 7/part1.py
+++ 7/part1.py
@@ -2,7 +2,7 @@
 hands = []
 for line in open('data.txt', 'r').readlines():
     hand = line.split(' ')
-    hands.append((hand[0].replace('A', 'a').replace('K', 'b').replace('Q', 'c').replace('J', 'd').replace('T', 'e').replace('9', 'f').replace('8', 'g').replace('7', 'h').replace('6', 'i').replace('5', 'j').replace('4', 'k').replace('3', 'l').replace('2', 'm'), int(hand[1].rstrip('\n'))))
+    hands.append((hand[0].replace('A', 'm').replace('K', 'l').replace('Q', 'k').replace('J', 'j').replace('T', 'i').replace('9', 'h').replace('8', 'g').replace('7', 'f').replace('6', 'e').replace('5', 'd').replace('4', 'c').replace('3', 'b').replace('2', 'a'), int(hand[1].rstrip('\n'))))
 
 sortedHands = [[], [], [], [], [], [], []]
 
@@ -17,25 +17,25 @@ for hand in hands:
 
     for cardCount in cardCounts:
         if cardCount[1] == 5:
-            sortedHands[0].append(hand)
+            sortedHands[6].append(hand)
             break
         if cardCount[1] == 4:
-            sortedHands[1].append(hand)
+            sortedHands[5].append(hand)
             break
         if cardCount[1] == 3:
             if len(cardCounts) == 2:
-                sortedHands[2].append(hand)
+                sortedHands[4].append(hand)
             else:
                 sortedHands[3].append(hand)
             break
         if cardCount[1] == 2:
             if len(cardCounts) == 3:
-                sortedHands[4].append(hand)
+                sortedHands[2].append(hand)
             else:
-                sortedHands[5].append(hand)
+                sortedHands[1].append(hand)
             break
     else:
-        sortedHands[6].append(hand)
+        sortedHands[0].append(hand)
 
 result = 0
 i = 1
