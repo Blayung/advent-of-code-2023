9bc0fa9e1937f34b62e8cf4a7c166f2ce6dd91de
Blayung <gpewojan1@gmail.com>
2023-12-10 22:25:50 +0100

    sth in direction of solving the first half of the 10th puzzle; I feel very little motivation to finish the previous puzzles (and this one too)

---
+++ 10/part1.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+from sys import setrecursionlimit
+
+setrecursionlimit(2147483647)
+
+map = open('data.txt', 'r').readlines()
+
+def processField(currentPos, cameFrom, step):
+    if map[currentPos[0]][currentPos[1]] == 'S':
+        return step / 2
+    if cameFrom == (currentPos[0], currentPos[1] - 1):
+        if map[currentPos[1]][currentPos[0]] == '|':
+            return processField((currentPos[0], currentPos[1] + 1), currentPos, step + 1)
+        if map[currentPos[1]][currentPos[0]] == 'J':
+            return processField((currentPos[0] - 1, currentPos[1]), currentPos, step + 1)
+        if map[currentPos[1]][currentPos[0]] == 'L':
+            return processField((currentPos[0] + 1, currentPos[1]), currentPos, step + 1)
+    if cameFrom == (currentPos[0], currentPos[1] + 1):
+        if map[currentPos[1]][currentPos[0]] == '|':
+            return processField((currentPos[0], currentPos[1] - 1), currentPos, step + 1)
+        if map[currentPos[1]][currentPos[0]] == '7':
+            return processField((currentPos[0] - 1, currentPos[1]), currentPos, step + 1)
+        if map[currentPos[1]][currentPos[0]] == 'F':
+            return processField((currentPos[0] + 1, currentPos[1]), currentPos, step + 1)
+    if cameFrom == (currentPos[0] - 1, currentPos[1]):
+        if map[currentPos[1]][currentPos[0]] == '-':
+            return processField((currentPos[0] + 1, currentPos[1]), currentPos, step + 1)
+        if map[currentPos[1]][currentPos[0]] == 'J':
+            return processField((currentPos[0], currentPos[1] - 1), currentPos, step + 1)
+        if map[currentPos[1]][currentPos[0]] == '7':
+            return processField((currentPos[0], currentPos[1] + 1), currentPos, step + 1)
+    if cameFrom == (currentPos[0] + 1, currentPos[1]):
+        if map[currentPos[1]][currentPos[0]] == '-':
+            return processField((currentPos[0] - 1, currentPos[1]), currentPos, step + 1)
+        if map[currentPos[1]][currentPos[0]] == 'L':
+            return processField((currentPos[0], currentPos[1] - 1), currentPos, step + 1)
+        if map[currentPos[1]][currentPos[0]] == 'F':
+            return processField((currentPos[0], currentPos[1] + 1), currentPos, step + 1) 
+
+print(processField((101, 95), (101, 96), 1))
+print(processField((102, 96), (101, 96), 1))
