7beb35d8450a7b5510b6eb3e620a0fba34ff0e33
Blayung <gpewojan1@gmail.com>
2023-12-13 20:03:48 +0100

    finished (yes, actually finished!) the first half of the 10th puzzle

--- 10/part1.py
+++ 10/part1.py
@@ -6,6 +6,12 @@ setrecursionlimit(2147483647)
 map = open('data.txt', 'r').readlines()
 
 def processField(currentPos, cameFrom, step):
+    #for y, line in enumerate(map):
+    #    for x, char in enumerate(line):
+    #        print('#' if currentPos == (x, y) else char, end = '')
+    #print(step)
+    #input()
+
     if map[currentPos[0]][currentPos[1]] == 'S':
         return step / 2
     if cameFrom == (currentPos[0], currentPos[1] - 1):
@@ -35,7 +41,9 @@ def processField(currentPos, cameFrom, step):
         if map[currentPos[1]][currentPos[0]] == 'L':
             return processField((currentPos[0], currentPos[1] - 1), currentPos, step + 1)
         if map[currentPos[1]][currentPos[0]] == 'F':
-            return processField((currentPos[0], currentPos[1] + 1), currentPos, step + 1) 
+            return processField((currentPos[0], currentPos[1] + 1), currentPos, step + 1)
 
-print(processField((101, 95), (101, 96), 1))
-print(processField((102, 96), (101, 96), 1))
+#print(processField((101, 95), (101, 96), 1))
+#print(processField((102, 96), (101, 96), 1))
+# I don't know why summing these up works, but I do not really care xd
+print(processField((101, 95), (101, 96), 1) + processField((102, 96), (101, 96), 1))
